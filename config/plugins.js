25cdd60e091a24e179dc8df36966d8898907e6d3
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2024-03-01 12:59:55 -0600

    update

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = {}
+module.exports = {
+  'strapi-summarize-ai-plugin': {
+    enabled: true,
+    config: {
+      openAIApiKey: process.env.OPEN_AI_API_KEY,
+      model: process.env.OPEN_AI_MODEL,
+      maxTokens: process.env.MAX_TOKENS,
+      temp: process.env.TEMP,
+    },
+  },
+}
